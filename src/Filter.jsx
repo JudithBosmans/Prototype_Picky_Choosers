7742317df4e3ec57c954fe2aa33b421d9eb5259a
Judith Bosmans <judobosmans@gmail.com>
2023-04-27 16:00:06 +0200

    Prototype_Picky_Choosers WIP
    Displays buttons that show different movies when pressed.

---
+++ src/Filter.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import PollCard from "./Filter";
+import { dummyData } from "./data";
+
+function PollList() {
+  // Maakt de array polls and vult die me data dummyData
+  const [polls] = useState(dummyData);
+  const [loading] = useState(false);
+
+  // Drei functies hieronder filteren polls data met find method, passen da nr setshowpolls en die zetten showpolls variable nr een array die da object heeft
+  function showA() {
+    const A = polls.find((e) => e.name === "Ponyo");
+    setShowPolls([A]);
+  }
+
+  function showB() {
+    const B = polls.filter((e) => e.name === "Spirited Away");
+    setShowPolls(B);
+  }
+
+  function showC() {
+    const C = polls.filter((e) => e.name === "My Neighbour Totoro");
+    setShowPolls(C);
+  }
+
+  // State om data te filteren en showpolls te updaten
+  const [showPolls, setShowPolls] = useState([]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // displayed A, B of C wnr geclicked. Gebruikt map function om PollCard te renderen. Rendert div me info uit eerste object in showpolss array. "?." operator
+  return (
+    <div className="polls">
+      <button onClick={() => showA()}>A</button>
+      <button onClick={() => showB()}>B</button>
+      <button onClick={() => showC()}>C</button>
+      {showPolls.map((poll) => (
+        <div key={poll.name}>
+          <PollCard poll={poll} />
+          <hr style={{ opacity: ".1" }} />
+        </div>
+      ))}
+
+      <div className="card">
+        <h1>{showPolls[0]?.name}</h1>
+        <h2>{showPolls[0]?.director}</h2>
+        <img src={showPolls[0]?.img} />
+      </div>
+    </div>
+  );
+}
+
+export default PollList;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const dummyData = [
+  {
+    name: "Ponyo",
+    director: "Hayao Miyazaki",
+    img: "https://media.npr.org/assets/artslife/movies/2009/08/ponyo/float-9b12df8eae8c7e00b19cb1a859a8269072d4095a-s1100-c50.jpg",
+  },
+  {
+    name: "Spirited Away",
+    director: "Hayao Miyazaki",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlCPSJykiI937Ga7oz88yu8f2NJr29GJhZmy0kdUzCqcXBgpUJJfcZoWjOdApgFLN3wGg&usqp=CAU",
+  },
+  {
+    name: "My Neighbour Totoro",
+    director: "Hayao Miyazaki",
+    img: "https://m.media-amazon.com/images/M/MV5BMTg1NzkyNDk4N15BMl5BanBnXkFtZTgwMDE2MDIyMDE@._V1_.jpg",
+  },
+];
